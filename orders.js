a745c7f5fd8ff2bacbc1f791e56196890aa34484
DK-0101 <Dayksontavares90@gmail.com>
2023-07-06 14:20:04 -0300

    analytics-app

---
+++ orders.js
@@ -0,0 +1,24 @@
+const Orders = [
+    {
+        productName: 'JavaScript',
+        productNumber: '249284',
+        paymentStatus: 'Paid',
+        status: 'Pending'
+    },
+
+
+    {
+        productName: 'CSS Course',
+        productNumber: '1743',
+        paymentStatus: 'Due',
+        status: 'Declined'
+    },
+    
+
+    {
+        productName: 'Flex-Box',
+        productNumber: '5332',
+        paymentStatus: 'Refunded',
+        status: 'Active'
+    },
+]
